bf253e8e90dc8ca7a9e51984efe89816b7303686
lakshmankolpe <lakshmankolpe912@gmail.com>
2024-09-08 23:30:44 +0530

    flower add

--- src/config/data.js
+++ src/config/data.js
@@ -1,77 +1,65 @@
-const MAIN_TITLE = "Chatpata Corner 🍔🍕";
-const TAGLINE = "best Food in The City.....!🍔🍿🌭🥪";
+const MAIN_TITLE = "Pushp Vatika💐🌻 ";
+const TAGLINE =
+  "Fresh flowers for every occasion at your your doorstep ...!💐🌹🌸 ";
 const THEME = {
-    Main:"#eff0f3",
-    secondry:"#fffffe",
-    Highlight:'#ff8e3c',
-    tertiary:'#d9376e',
-    dark:"#0d0d0d"
-}
+  Main: "#ffe0cc",
+  secondry: "#fffffe",
+  Highlight: "#ff944d",
+  tertiary: "#d9376e",
+  dark: "#0d0d0d",
+};
 
-const foods = [
+const CARD_DIRECTION = "column"
+const CARD_BUTTON_TEXT = "By Now"
+
+const flowers = [
   {
-    title: "Pizza",
+    title: "Rose",
     imgUrl:
-      "https://i0.wp.com/picjumbo.com/wp-content/uploads/yummy-and-tasty-salami-pizza-diavola-close-up-free-photo.jpg?w=600&quality=80",
-    price: "20.00",
-    description: "Your city's favorite pizza place, and you can order online.",
-    isVeg: true,
-  },
-  {
-    title: "Matar Kulcha",
-    imgUrl:
-      "https://res.cloudinary.com/hz3gmuqw6/image/upload/c_fill,q_60,w_750,f_auto/11-matar-kulcha-canva-phpiqTrzB",
-    price: "20.00",
-    description: "your city's favorite pizza place.and you can order online.",
-    isVeg: true,
-  },
-  {
-    title: "Fish Moilee",
-    imgUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPvnoRWez9bDkZdM-cG7k-v8b9TZLqX-5GkQ&s",
-    price: "15.00",
-    description: "my city'S favorite fish place. and you can order online.",
-    isVeg: false,
-  },
-  {
-    title: "Burger",
-    imgUrl:
-      "https://img.freepik.com/free-photo/big-sandwich-hamburger-burger-with-beef-red-onion-tomato-fried-bacon_2829-5398.jpg?t=st=1725739370~exp=1725742970~hmac=d1b82657ead82fdffc631a466657a13c061fff10b86ee22de1600fddebd1f0c5&w=740",
-    price: "15.00",
-    description: "your city'S favorite burger place. and you can order online.",
-    isVeg: true,
-  },
-  {
-    title: "Egg Curry",
-    imgUrl:
-      "https://img.freepik.com/premium-photo/delicious-indian-cuisine-egg-curry-recipe-with-boiled-eggs_136354-18430.jpg?ga=GA1.1.215319620.1706536294&semt=ais_hybrid",
-    price: "150.00",
-    description: "your city'S favorite fish place. and you can order online.",
-    isVeg: false,
-  },
-  {
-    title: "Veg Biryani",
-    imgUrl:
-      "https://img.freepik.com/free-photo/side-view-rice-garnish-with-fried-onion-carrot-greens-chili-pepper-table_141793-5069.jpg?ga=GA1.1.215319620.1706536294&semt=ais_hybrid",
-    price: "180.00",
-    description: "your city'S favorite fish place. and you can order online.",
-    isVeg: true,
-  },
-  {
-    title: "veg momos",
-    imgUrl: "https://www.bigbasket.com/media/uploads/recipe/w-l/3506_2_1.jpg",
-    price: "130.00",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXVZZliLgkKIp9zRtP5U2iVR6X3-9BPknXSg&s",
+    price: "110.00",
     description:
-      "your city'S favorite fish place. and you can order online.",
-    isVeg: true,
+      "Roses are one of the most loved flowers in the world. Roses are one of the most loved flowers in the world.",
   },
   {
-    title:"Chicken Biryani",
-    imgUrl:"https://img.freepik.com/premium-photo/chicken-with-rice-plate_1205263-158399.jpg?ga=GA1.1.215319620.1706536294&semt=ais_hybrid",
-    price:"180.00",
-    description: "your city'S favorite fish place. and you can order online.",
-    isVeg: false,
+    title: "Lily",
+    imgUrl:
+      "https://img.freepik.com/premium-photo/blooming-pink-lily-green-background_3510-6431.jpg?ga=GA1.1.215319620.1706536294&semt=ais_hybrid",
+    price: "80.00",
+    description:
+      "Lilies are a type of flowering plant. They have long stems and can grow to be several feet tall. ",
+  },
+  {
+    title: "Orchid",
+    imgUrl:
+      "https://img.freepik.com/free-photo/close-up-beautiful-orchids_23-2150960619.jpg?ga=GA1.1.215319620.1706536294&semt=ais_hybrid",
+    price: "120.00",
+    description:
+      "Orchids are flowering plants that produce a large number of small flowers on the stem. Orchid flowers have a wide range of colors and shapes. ",
+  },
+  {
+    title: "Carnation",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT4yQ9HKRTRE-FrVbhsxZioHvUTBC86Zay7A&s",
+    price: "210.00",
+    description:
+      "Carnations are a type of flowering plant that produces small flowers on the stem. The carnation flower has a wide range of colors and shapes.",
+  },
+  {
+    title: "Aster",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiCyZCvvdp_tdp6ei6Nw4mPQjFK-WctaIcdA&s",
+    price: "90.00",
+
+    description:
+      "The aster is a flowering plant that produces small flowers on the stem. The aster flower has a wide range of colors and shapes. ",
+  },
+  {
+    title:"Toran",
+    imgUrl:"https://img.freepik.com/premium-vector/decorative-toran-made-fresh-flowers-leaves_978445-792.jpg?w=740",
+    price: "350.00",
+    description:"Traditionally, torans are a work of domestic embroidery. They might be a part of a woman's dowry, "
   }
 ];
 
-export { MAIN_TITLE, foods, TAGLINE,THEME };
+export { MAIN_TITLE, flowers, TAGLINE, THEME,CARD_DIRECTION,CARD_BUTTON_TEXT };
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./Home.css";
 import FoodCard from "./../../components/FoodCard/FoodCard";
-import { foods,MAIN_TITLE,TAGLINE,THEME } from "../../config/data";
+import { flowers,MAIN_TITLE,TAGLINE,THEME } from "../../config/data";
 
 function Home() {
   return (
     <div className="home-container" style={{ backgroundColor: THEME.Main}}>
       <h1 className="main-title" style={{ color: THEME.Highlight,margin:0} }>{MAIN_TITLE}</h1>
       <p className="tagline-title">{TAGLINE}</p>
+      
       <div className="food-card-container">
-        {foods.map((fooditem) => {
+        {flowers.map((fooditem) => {
           return (
             <FoodCard
               title={fooditem.title}
               imgUrl={fooditem.imgUrl}
               price={fooditem.price}
               description={fooditem.description}
-              isVeg={fooditem.isVeg}
             />
           );
         })}
